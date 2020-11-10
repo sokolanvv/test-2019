7bf05d9b311932ddb6d79c9fc72f7747a7853c27
sokolanvv <sokolan.vvvv@gmail.com>
2020-11-11 00:28:46 +1000

    Sorting by (name+lastname) was added

--- frontend/users/src/components/desktop/List.tsx
+++ frontend/users/src/components/desktop/List.tsx
@@ -8,77 +8,81 @@ interface Props {
   intl: InjectedIntl
 }
 
-const List = ({ rows, intl }: Props) => (
-  <Column>
-    <Layout basis={60} />
-    <Row>
-      <Layout basis='10%' />
-      <Text weight='medium' size='l'>
-        {intl.formatMessage(messages.users)}
-      </Text>
-      <Layout basis='10%' />
-    </Row>
-    <Layout basis={20} />
-    <Row>
-      <Layout basis='10%' />
-      <Layout basis={300}>
-        <Layout basis={8} />
-        <Text size='s' weight='bold' transform='uppercase'>
-          {intl.formatMessage(messages.name)}
+const List = ({ rows, intl }: Props) => {
+  rows = rows.sort((a, b) => 
+    (a.profile.firstName + a.profile.lastName) < (b.profile.firstName + b.profile.lastName) ? 1 : -1 )
+
+  return (
+    <Column>
+      <Layout basis={60} />
+      <Row>
+        <Layout basis='10%' />
+        <Text weight='medium' size='l'>
+          {intl.formatMessage(messages.users)}
         </Text>
-      </Layout>
-      <Layout basis={200}>
-        <Text size='s' weight='bold' transform='uppercase'>
-          {intl.formatMessage(messages.email)}
-        </Text>
-      </Layout>
-      <Layout basis={180}>
-        <Text size='s' weight='bold' transform='uppercase'>
-          {intl.formatMessage(messages.registered)}
-        </Text>
-      </Layout>
-      <Layout basis={160}>
-        <Text size='s' weight='bold' transform='uppercase'>
-          {intl.formatMessage(messages.lastLogin)}
-        </Text>
-      </Layout>
-      <Layout basis='10%' />
-    </Row>
-    <Layout basis={8} />
-    {rows.map(({ id, profile, email, registeredAt, lastLogonAt }) => (
-      <Fragment key={id}>
-        <Row>
-          <Layout basis='10%' />
+        <Layout basis='10%' />
+      </Row>
+      <Layout basis={20} />
+      <Row>
+        <Layout basis='10%' />
+        <Layout basis={300}>
           <Layout basis={8} />
-          <Layout basis={280}>
-            <Text size='s'>
-              {profile.firstName}
-              <Space />
-              {profile.lastName}
-            </Text>
-          </Layout>
-          <Layout basis={12} />
-          <Layout basis={188}>
-            <Text size='s'>{email}</Text>
-          </Layout>
-          <Layout basis={12} />
-          <Layout basis={168}>
-            <Text size='s'>{intl.formatDate(registeredAt)}</Text>
-          </Layout>
-          <Layout basis={12} />
-          <Text size='s'>
-            {intl.formatDate(lastLogonAt)}
-            <Space />
-            {intl.formatMessage(messages.at)}
-            <Space />
-            {intl.formatTime(lastLogonAt)}
+          <Text size='s' weight='bold' transform='uppercase'>
+            {intl.formatMessage(messages.name)}
           </Text>
-          <Layout basis='10%' />
-        </Row>
-        <Layout basis={12} />
-      </Fragment>
-    ))}
-  </Column>
-)
+        </Layout>
+        <Layout basis={200}>
+          <Text size='s' weight='bold' transform='uppercase'>
+            {intl.formatMessage(messages.email)}
+          </Text>
+        </Layout>
+        <Layout basis={180}>
+          <Text size='s' weight='bold' transform='uppercase'>
+            {intl.formatMessage(messages.registered)}
+          </Text>
+        </Layout>
+        <Layout basis={160}>
+          <Text size='s' weight='bold' transform='uppercase'>
+            {intl.formatMessage(messages.lastLogin)}
+          </Text>
+        </Layout>
+        <Layout basis='10%' />
+      </Row>
+      <Layout basis={8} />
+      {rows.map(({ id, profile, email, registeredAt, lastLogonAt }) => (
+        <Fragment key={id}>
+          <Row>
+            <Layout basis='10%' />
+            <Layout basis={8} />
+            <Layout basis={280}>
+              <Text size='s'>
+                {profile.firstName}
+                <Space />
+                {profile.lastName}
+              </Text>
+            </Layout>
+            <Layout basis={12} />
+            <Layout basis={188}>
+              <Text size='s'>{email}</Text>
+            </Layout>
+            <Layout basis={12} />
+            <Layout basis={168}>
+              <Text size='s'>{intl.formatDate(registeredAt)}</Text>
+            </Layout>
+            <Layout basis={12} />
+            <Text size='s'>
+              {intl.formatDate(lastLogonAt)}
+              <Space />
+              {intl.formatMessage(messages.at)}
+              <Space />
+              {intl.formatTime(lastLogonAt)}
+            </Text>
+            <Layout basis='10%' />
+          </Row>
+          <Layout basis={12} />
+        </Fragment>
+      ))}
+    </Column>
+)}
 
 export default injectIntl(List)
